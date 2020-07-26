48bf81a5d7fe9500f5098fd966a9a40305a7534e
austinckohler <austinckohler@gmail.com>
2020-07-26 12:18:17 -0600

    added instructions for project creation

--- index.js
+++ index.js
@@ -1,57 +1,18 @@
-const shoe = {
-  brand: "Nike",
-  shoe: "Jordan's",
-  color: "white",
-};
+// 11 steps for this advanced quiz
 
-let Store = function (brand, shoe, color) {
-  this.brand = brand;
-  this.shoe = shoe;
-  this.color = color;
-};
-// ES 2015 way
-// class Store {
-//     constructor(brand, shoe, color) {
-//         this.brand = brand;
-//         this.shoe = shoe;
-//         this.color = color;
-//     }
-// }
-Store.prototype.brandAndAge = function () {
-  console.log(this.brand + " " + this.shoe);
-};
-const shoe1 = new Store("Merril", "Slip On", "brown");
-const shoe2 = new Store("La Sportiva", "climber", "yellow");
-const shoe3 = new Store("Nike", "runner", "grey and white");
-shoe1.brandAndAge();
-shoe2.brandAndAge();
-shoe3.brandAndAge();
+// 1. Build a function constructor called Question to describe a question. A question should include:
+// a) question itself
+// b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+// c) correct answer using a number
+// 2. Create a couple of questions using the constructor
+// 3. Store them all inside an array
+// 4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
+// 5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+// 6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+// 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+// 8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+// 9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+// 10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+// 11. Display the score in the console. Use yet another method for this.
 
-let dogs = ["rosie", "sandy", "rhoady"];
-function doogAges(dogs, func) {
-  let dogAge = [];
-  for (let index = 0; index < dogs.length; index++) {
-    dogAge.push(func(dogs[index]));
-  }
-  return dogAge;
-}
-
-// function ages(element) {
-//   return element[0] + "is a 5";
-// }
-
-// let otherAges = doogAges(dogs, ages);
-// console.log(otherAges);
-
-function dogQuestions(dog) {
-  return function (age) {
-    if (dog === "rosie") {
-      console.log(dog + " I am " + age + "years old");
-    } else {
-      console.log(dog + ", are you");
-    }
-  };
-}
-
-// dogQuestions("rosie")(5);
-dogQuestions("sandy");
+function 
